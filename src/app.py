18aa428d884c1c2336ebc1406dcab0d308b4e2c5
greerda <david.greer2@gmail.com>
2024-04-07 18:42:22 -0400

    Added SQLlite 3 and created tables based on fields. Has PK-FK relationships

--- src/app.py
+++ src/app.py
@@ -1,4 +1,65 @@
 import streamlit as st
+import sqlite3
+
+conn = sqlite3.connect(':memory:')
+cursor_obj = conn.cursor()
+
+cursor_obj.execute("DROP TABLE IF EXISTS PATIENT")
+cursor_obj.execute("DROP TABLE IF EXISTS STUDY")
+cursor_obj.execute("DROP TABLE IF EXISTS WEIGHT")
+cursor_obj.execute("DROP TABLE IF EXISTS HEIGHT")
+cursor_obj.execute("DROP TABLE IF EXISTS VISIT")
+
+cursor_obj.execute('''CREATE TABLE IF NOT EXISTS PATIENT (
+                PK_Patient_ID INTEGER PRIMARY KEY,
+                FK_Patient_Weight_Units_ID INTEGER NOT NULL,
+                FK_Patient_Height_Units_ID INTEGER NOT NULL,
+                FK_VISIT_ID INTEGER NOT NULL,
+                FK_STUDY_ID INTEGER NOT NULL,
+                Patient_Init TEXT NOT NULL,
+                Patient_Weight INTEGER NOT NULL,                
+                Patient_Height INTEGER NOT NULL,                
+                Patient_DOB TEXT NOT NULL,
+                Patient_Age INTEGER NOT NULL,
+                PEDIATRIC_DOB TEXT,
+                STREET_ADDRESS TEXT,               
+                FOREIGN KEY(FK_STUDY_ID) REFERENCES STUDY(PK_STUDY_ID),
+                FOREIGN KEY(FK_VISIT_ID) REFERENCES VISIT(PK_VISIT_ID),
+                FOREIGN KEY(FK_Patient_Weight_Units_ID) REFERENCES WEIGHT_UNITS(PK_WEIGHT_UNITS_ID),
+                FOREIGN KEY(FK_Patient_Height_Units_ID) REFERENCES WEIGHT_UNITS(PK_HEIGHT_UNITS_ID)
+            )''')
+
+cursor_obj.execute('''CREATE TABLE IF NOT EXISTS HEIGHT_UNITS (
+                PK_HEIGHT_UNITS_ID INTEGER PRIMARY KEY,
+                HEIGHT_UNITS TEXT NOT NULL
+            )''')
+
+cursor_obj.execute('''CREATE TABLE IF NOT EXISTS WEIGHT_UNITS (
+                PK_WEIGHT_UNITS_ID INTEGER PRIMARY KEY,
+                WEIGHT_UNITS TEXT NOT NULL
+            )''')
+
+cursor_obj.execute('''CREATE TABLE IF NOT EXISTS VISIT (
+                PK_VISIT_ID INTEGER PRIMARY KEY,
+                VISIT_CODE TEXT NOT NULL,
+                FIRST_MORNING_VOID TEXT NOT NULL,
+                SITE_NURSE_SIGNATURE  TEXT NOT NULL,
+                COLLECTION_DATE TEXT NOT NULL,
+                COLLECTION_TIME TEXT NOT NULL,
+                PREGNANCY TEXT NOT NULL,
+                HEMOGLOBIN_A1C TEXT NOT NULL,
+                FASTING_STATUS  TEXT NOT NULL,
+                OPTIONAL_TESTING TEXT NOT NULL
+            )''')
+
+cursor_obj.execute('''CREATE TABLE IF NOT EXISTS STUDY (
+                PK_STUDY_ID INTEGER PRIMARY KEY,
+                COLLECTION_DATE TEXT NOT NULL,
+                COLLECTION_TIME TEXT NOT NULL,
+                SITE_NURSE_SIGNATURE  TEXT NOT NULL
+            )''')
+conn.commit()
+conn.close()
 
 # Title of the web page
 st.title('Customer Support Form')
@@ -17,8 +78,6 @@ if st.button('Submit'):
     # You can process the form data here
     st.success(f"Your problem of type '{problem_type}' has been submitted successfully!")
     st.write(f"Problem Description: {problem_description}")
-
-
 ## from the virtualenv activated directory, run..
-# streamlit run ./src/app.py 
+#
 
